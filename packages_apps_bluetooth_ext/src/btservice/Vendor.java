db5c4f703a3884c465ed92441684ee45e52847bd
M Safoorah Banu <safoorah@codeaurora.org>
2019-01-25 16:16:01 +0530

    BT: Close HCI before force kill BT process[2/3]
     - In Timeout cases forcefully killing BT process
       leads to HAL daemon stuck in binder threads
       which results in spurious wake timeout.
    
     - This patch will close hci gracefully before
       killing BT process to avoid spurious wake timeouts.
    
    CRs-Fixed: 2393499
    
    Change-Id: Id53fe4931fcdc363970c9a71aeebaa47adb3835d

--- packages_apps_bluetooth_ext/src/btservice/Vendor.java
+++ packages_apps_bluetooth_ext/src/btservice/Vendor.java
@@ -88,6 +88,10 @@ final class Vendor {
         setWifiStateNative(status);
     }
 
+    public void HCIClose() {
+        hcicloseNative();
+    }
+
     public boolean getProfileInfo(int profile_id , int profile_info) {
         Log.d(TAG,"getProfileInfo profile_id: " + profile_id);
         return getProfileInfoNative(profile_id, profile_info);
@@ -216,4 +220,5 @@ final class Vendor {
     private native boolean getProfileInfoNative(int profile_id , int profile_info);
     private native boolean getQtiStackStatusNative();
     private native boolean voipNetworkWifiInfoNative(boolean isVoipStarted, boolean isNetworkWifi);
+    private native void hcicloseNative();
 }
